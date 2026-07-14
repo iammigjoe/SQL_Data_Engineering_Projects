22b00290c83b1fd650cb184b96ee0458dc126786
ABRAHAM MIGUEL TALA <133469839+iammigjoe@users.noreply.github.com>
2026-07-14 14:20:46 +0200

    data validation check sat

--- 2_DW_Mart_Build/02_load_schema_dw.sql
+++ 2_DW_Mart_Build/02_load_schema_dw.sql
@@ -38,3 +38,23 @@ INSERT INTO skills_job_dim (skill_id, job_id)
 SELECT skill_id, job_id
 FROM read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv',
     AUTO_DETECT=true);
+
+SELECT 'Company Dim' AS table_name, COUNT(*) AS record_count FROM company_dim
+UNION ALL
+SELECT 'Skills Dim', COUNT(*) FROM skills_dim
+UNION ALL
+SELECT 'Job Postings Fact', COUNT(*) FROM job_postings_fact
+UNION ALL
+SELECT 'Skills Job Dim', COUNT(*) FROM skills_job_dim;
+
+SELECT '=== Company Dimension Sample ===' AS Info;
+SELECT * FROM company_dim LIMIT 5;
+
+SELECT '=== Skills Dimension Sample ===' AS Info;
+SELECT * FROM skills_dim LIMIT 5;
+
+SELECT '=== Job Postings Fact Sample ===' AS Info;
+SELECT * FROM job_postings_fact LIMIT 5;
+
+SELECT '=== Skills Job Bridge Sample ===' AS Info;
+SELECT * FROM skills_job_dim LIMIT 5;
